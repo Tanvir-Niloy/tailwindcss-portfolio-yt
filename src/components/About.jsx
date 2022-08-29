239fd62356081656e177a494103c2e23133112fa
Tanvir-Niloy <tanvirniloy428@gmail.com>
2022-08-30 00:37:13 +0600

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about'
+    className='w-full h-screen bg-gradient-to-b from-gray-800
+ to-black text-white'>
+     <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold
+            inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-16'>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit in deserunt, assumenda distinctio consectetur possimus, aut illum esse laborum ipsum ut officiis ad sit voluptatem labore. Cum veritatis labore et suscipit a ipsam hic, temporibus dignissimos? Iste, porro omnis?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate architecto ipsa ipsam voluptas aliquam omnis perferendis, eum suscipit ratione rem?
+        </p>
+        <br />
+        <p className='text-xl'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, deserunt. Ducimus, explicabo nam! Repudiandae aspernatur veritatis quis atque tenetur, maiores eos sequi quia cumque ipsam rem architecto. Quas cum error inventore laudantium, minima commodi, quod adipisci officia, pariatur esse iusto earum ipsum aut recusandae distinctio in nesciunt suscipit corrupti minus.
+        </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
